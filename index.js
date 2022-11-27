c0329fee3bac69820c844f0fa142b6c5361c5d70
mushfik16381 <ahmed.mushfik16381@gmail.com>
2022-11-27 19:13:57 +0600

    get category

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,6 +11,28 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hid3uqt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        const categoryCollection = client.db('bookshop').collection('category');
+
+        app.get('/category', async(req, res) =>{
+            const query = {};
+            const cursor = categoryCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    }
+    finally{
+    }
+}
+run().catch(err => console.error(err));
+
+
 
 
 app.get('/', (req, res) =>{
